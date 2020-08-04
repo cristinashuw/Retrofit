21ec55f8070e178062d305b19bdc5f10531cac2c
cristinashuw <cristinashuw@gmail.com>
2020-08-04 16:45:13 +0700

    Get service

---
+++ app/src/main/java/com/example/retrofit/CustomAdapter.java
@@ -0,0 +1,64 @@
+package com.example.retrofit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.retrofit.model.PhotoData;
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+
+    private List<PhotoData> dataList;
+    private Context context;
+
+    public CustomAdapter(Context context,List<PhotoData> dataList){
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        public final View mView;
+
+        private ImageView coverImage;
+
+        CustomViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+
+            coverImage = mView.findViewById(R.id.coverImage);
+        }
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.custom_row, parent, false);
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder holder, int position) {
+
+        Picasso.Builder builder = new Picasso.Builder(context);
+        builder.downloader(new OkHttp3Downloader(context));
+        builder.build().load(dataList.get(position).getThumbnailUrl())
+                .placeholder((R.drawable.ic_launcher_background))
+                .error(R.drawable.ic_launcher_background)
+                .into(holder.coverImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.retrofit.model.PhotoData;
+import com.example.retrofit.service.ApiClient;
+import com.example.retrofit.service.GetService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CustomAdapter adapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        GetService service = ApiClient.getRetrofitInstance().create(GetService.class);
+        Call<List<PhotoData>> call = service.getAllPhotos();
+        call.enqueue(new Callback<List<PhotoData>>() {
+            @Override
+            public void onResponse(Call<List<PhotoData>> call, Response<List<PhotoData>> response) {
+                progressDialog.dismiss();
+                generateDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<PhotoData>> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    /*Method to generate List of data using RecyclerView with custom adapter*/
+    private void generateDataList(List<PhotoData> photoList) {
+        recyclerView = findViewById(R.id.customRecyclerView);
+        adapter = new CustomAdapter(this,photoList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+
 }
