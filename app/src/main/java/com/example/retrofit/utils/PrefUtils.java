314e945b8fd57f8be2c8b3812c021abc75ed29fd
cristinashuw <cristinashuw@gmail.com>
2020-08-13 12:49:11 +0700

    add class PrefUtils

---
+++ app/src/main/java/com/example/retrofit/utils/PrefUtils.java
@@ -0,0 +1,26 @@
+package com.example.retrofit.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+public class PrefUtils {
+    /**
+     * Storing API Key in shared preferences to
+     * add it in header part of every retrofit request
+     */
+    public PrefUtils() {
+    }
+
+    private static SharedPreferences getSharedPreferences(Context context) {
+        return context.getSharedPreferences("APP_PREF", Context.MODE_PRIVATE);
+    }
+
+    public static void storeApiKey(Context context, String apiKey) {
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putString("API_KEY", apiKey);
+        editor.apply();
+    }
+
+    public static String getApiKey(Context context) {
+        return getSharedPreferences(context).getString("API_KEY", null);
+    }
+}
